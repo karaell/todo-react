745070122aff18b5675e53aada2ff9962da15cdc
karaell <elenakara2@gmail.com>
2022-03-16 18:12:50 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import { useState, useEffect } from 'react'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 import { PriorityTitle } from './Title';
 
-import add from "./image/add.svg";
-import del from "./image/delete.svg"
+import add from './image/add.svg';
+import del from './image/delete.svg';
 
 const PRIORITY = {
-    HIGH: "high",
-    LOW: "low",
-}
+    HIGH: 'high',
+    LOW: 'low',
+};
 
-const TASKS_HIGH = [];
-const TASKS_LOW = [];
+const TASKS = [];
+
+let count = 1;
 
 function App() {
     return (
@@ -19,93 +20,128 @@ function App() {
             <PriorityBlock priority={PRIORITY.HIGH} />
             <PriorityBlock priority={PRIORITY.LOW} />
         </div>
-    )
+    );
 }
 
 function PriorityBlock(props) {
-    const [taskListHigh, setTaskListHigh] = useState(TASKS_HIGH);
-    const [taskListLow, setTaskListLow] = useState(TASKS_LOW);
+    const [tasksList, setTasksList] = useState(TASKS)
 
     const priority = props.priority;
 
-    function handleTaskListHigh(newTaskText) {
-        setTaskListHigh([...taskListHigh, newTaskText]);
-    }    
-
-    function handleTaskListLow(newTaskText) {
-        setTaskListLow([...taskListLow, newTaskText]);
+    function handleTasksList(newTaskText) {
+        const newTask = {
+            done: false,
+            id: count,
+            value: newTaskText,
+        }
+        setTasksList([...tasksList, newTask]);
+        count++;
     }
 
-    let array;
+    function handleArrayChange(tasks) {
+        setTasksList(tasks);
 
-    if (priority === "high") { array = taskListHigh }
-    else { array = taskListLow }
+        console.log(tasksList)
+    }
 
     return (
         <div className="todo__priority-block">
             <PriorityTitle priority={priority} />
             <AddPanel 
                 priority={priority} 
-                onTaskListHigh={handleTaskListHigh}
-                onTaskListLow={handleTaskListLow}
+                onTasksList = {handleTasksList}
             />
-            <TasksList array={array}/>
+            <TasksList 
+                array={tasksList} 
+                onChangeTasksList={handleArrayChange}
+            />
         </div>
-    )
+    );
 }
 
 function AddPanel(props) {
-    const priority = props.priority;
-    
+    const [inputValue, setInputValue] = useState('')
+
+
     function handleSubmit(event) {
         event.preventDefault();
 
-        const newTaskText = event.target[0].value;
+        props.onTasksList(event.target[0].value);
 
-        if (priority === "high") {
-            return props.onTaskListHigh(newTaskText);
-        }
-        return props.onTaskListLow(newTaskText);
-        
+        setInputValue('')
+    }
+
+    function onChange(event) {
+        event.preventDefault();
+        setInputValue(event.target.value)
     }
 
     return (
-        <form className="todo__add-block" onSubmit={handleSubmit}>
-            <input 
-                // value={}
+      <form className="todo__add-block" onSubmit={handleSubmit}>
+          <input 
+                value={inputValue}
+                onChange = {onChange}
                 className="input-add" type="text" placeholder="Добавить важных дел" />
-            <button className="btn-add"> <img src={add} alt="Иконка добавить задачу"/> </button>
+          <button className="btn-add"> <img src={add} alt="Иконка добавить задачу"/> </button>
         </form>
-    )
+    );
 }
 
-function TasksList (props) {
+function TasksList(props) {
     const array = props.array;
-    console.log(array);
+
+
+    function handleDone(targetTaskId) {
+        // создаем копию массива, чтобы не мутировать исходный
+        const _array = array.concat();
+
+        // найлдем нужную задачу
+        const task = _array.find(task => task.id === targetTaskId);
+
+        // меняем свойство
+        task.done = task.done === true ? false : true ;
+
+        props.onChangeTasksList(_array)
+    }
+
+    function handleClose(targetTask) {
+        const newTasksArray = array.filter ( task => task.id !== targetTask.id)
+        props.onChangeTasksList(newTasksArray);
+    }
 
     return (
         <div className="tasksList">
-            {array.map (taskText => {
-                <TaskRow taskText = {taskText} />;
-            })}
+            {array.map (task => (
+                <TaskRow
+                    key = {task.id}
+                    task = {task} 
+                    onDone = {handleDone}
+                    onClose = {handleClose}
+                />
+            ))}
         </div>
-    )
-} 
+    );
+}
 
 function TaskRow(props) {
-    console.log(props.text)
-    const taskText = props.taskText;
+    const task = props.task;
+
+    const classes = ['todo__task'];
+
+    if (task.done) {
+        classes.push('done')
+    }
 
     return (
-        <div className="todo__task">
-            <label> 
-                <input className="input-done" type="checkbox"/> 
-                <span className="cursor"></span>
+      <div className={classes.join(' ')}>
+          <label> 
+              <input className="input-done" type="checkbox" onClick={ () => props.onDone (task.id) } /> 
+              <span className="cursor"></span>
             </label>
-            <span className="task-text" > {taskText} </span>
-            <button className="btn-delete"> <img src={del} alt="-"/> </button>
+          <span className="task-text" > {task.value} </span>
+          <button className="btn-delete" onClick={ () => props.onClose (task) } > <img src={del} alt="-"/> </button>
         </div>
-    )
+    );
 }
 
 export default App;
